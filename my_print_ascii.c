707c8d423d99f2b029f439a76672c235167a10ec
eminetrsn <167294728+eminetrsn@users.noreply.github.com>
2024-04-17 09:57:22 +0000

    ex2_reel

--- my_print_ascii.c
+++ my_print_ascii.c
@@ -11,7 +11,11 @@ void my_print_ascii(void)
     }
 }
 
-
+int main(void)
+ {
+ my_print_ascii();
+ return 0;
+ }
 
 
 
---
+++ my_print_n_ascii.c
@@ -0,0 +1,19 @@
+void print_char(char c)
+{
+    write(1, &c, 1);
+}
+
+
+
+void my_print_n_ascii(int howMany)
+{
+        if (33 < howMany <= 128 )
+        {
+        
+            for (int i = 33; i <= howMany; ++i)
+            {
+                print_char(i);
+
+            }
+        }
+}
